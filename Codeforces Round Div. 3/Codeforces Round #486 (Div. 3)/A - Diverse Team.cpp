92107c77d18ad114fd9cee23b4416439492e982a
Preetam2114 <preetamrane2114@gmail.com>
2020-11-08 23:18:53 +0530

    A - Diverse Team.cpp

---
+++ Codeforces Round Div. 3/Codeforces Round #486 (Div. 3)/A - Diverse Team.cpp
@@ -0,0 +1,70 @@
+/////////////////////// SET-UP ///////////////////////
+#include<bits/stdc++.h>
+#include <chrono> 
+#include <unistd.h> 
+#include <sys/resource.h>
+#include <errno.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+typedef long double ld;
+typedef long double f80;
+typedef pair<int, int> pii;
+const ll mod = 1e9;
+auto start = high_resolution_clock::now();  
+///////////////////// CODE HERE ///////////////////////
+
+
+//////////////////// ONLINE_JUDGE ////////////////////
+int main(){ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+    struct rusage r_usage;
+    int ret = getrusage(RUSAGE_SELF,&r_usage);
+///////////////////// CODE HERE ///////////////////////
+    ll t=1;
+    // cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+
+        unordered_map<int,int> m;
+        int i=1,num;
+        while(m.size()<k && i <= n){
+            cin>>num;
+            m[num]= i;
+            i++;
+        }
+        if(m.size()!=k){
+            cout<<"NO";
+            return 0;
+        }
+
+        cout << "YES" << endl;
+        for(auto a:m){
+            cout<<a.second<<" ";
+        }
+    } 
+//////////////////// System log //////////////////////
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout<<"----------------------------------------"<<endl;
+    std::cout << std::fixed << std::showpoint;
+    std::cout << std::setprecision(4);
+    cout << "Execution Time :  "<<float(duration.count()/1000000)
+            << " (seconds)" << endl;
+    cout<< "Process id     :  " 
+             << getpid() << endl;
+    cout<< "Memory usage   :  "<<r_usage.ru_maxrss<<endl;
+    cout<<"----------------------------------------"<<endl;
+//////////////////////////////////////////////////////
+    return 0; 
+} 
